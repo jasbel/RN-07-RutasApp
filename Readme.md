b3c81b042999bbf71d557699374af2b0ecd2820b
jasbel <asbel.elmec@gmail.com>
2021-06-22 08:29:37 -0400

    Maps

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 npx pod-install <!-- for IOS -->
 
 only test
+
+<!-- apimap AIzaSyC9FmjF7BM4lIbCZT3di4D99qEl2QceoyA -->
--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import MapView from 'react-native-maps';
 import { View, Text } from 'react-native'
 
 const MapScreen = () => {
     return (
-        <View>
-            <Text>Map Screen</Text>
+        <View style={{flex: 1}}>
+            <MapView
+                initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+                }}
+                style={{flex: 1}}
+            />
         </View>
     )
 }
